3cdc2940a9a508ceeed56371892924237f2ef16b
ranting <593087168@qq.com>
2023-04-14 17:11:59 +0800

    发布

--- playground/main.ts
+++ playground/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./app.vue";
-import rtui from "@rt-demo-ui/components";
+import rtui from "@rt-ui/components";
 const app = createApp(App);
 app.use(rtui);
 app.mount("#app");
